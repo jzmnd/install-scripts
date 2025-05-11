82aa228ebca02da4553e1f54de484da2cd8380a3
Jeremy Smith <j.smith.03@cantab.net>
2025-05-11 15:33:45 -0500

    Added fzf yq, brew install in groups, updated zshrc and iterm prefs

--- scripts/main-install.sh
+++ scripts/main-install.sh
@@ -22,106 +22,58 @@ if test ! $(which brew); then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 brew update
-brew upgrade
 
-# Install WGET
-brew install wget
+# Install core tools
+brew install wget gcc tmux cmake openssl
 
-# Install gcc and gfortran
-brew install gcc
-
-# Install tmux
-brew install tmux
-
-# Install cmake
-brew install cmake
+# GNU tools
+brew install coreutils findutils gnu-indent gnu-sed gnutls grep gnu-tar gawk
 
 # Git
-brew install git
-brew install git-flow
 brew tap b4nst/homebrew-tap
-brew install turbogit
+brew install git git-flow turbogit
 
 # Virtual envs
 pip install virtualenv
-brew install pyenv
-brew install pyenv-virtualenv
+brew install pyenv pyenv-virtualenv
+
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # GNU tools
-brew install coreutils
-brew install findutils
-brew install gnu-indent
-brew install gnu-sed
-brew install gnutls
-brew install grep
-brew install gnu-tar
-brew install gawk
+brew install coreutils findutils gnu-indent gnu-sed gnutls grep gnu-tar gawk
 
 # Install databases
-brew install mysql
-brew install postgresql@14
-brew install redis
+brew install mysql postgresql@14 redis
 curl https://install.duckdb.org | sh
 
 # Install scientific libraries
-brew install fftw
-brew install eigen
-brew install boost
+brew install fftw eigen boost
 
-# Rust
+# Rust, spark, scala, kafka, golang
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup-init
-
-# Golang
-brew install go
-
-# Install image software
-brew install gwyddion
-brew install epstool
-brew install imagemagick
-brew install graphviz
-
-# Audio tools
-brew install ffmpeg
-brew install libsndfile
-
-# Spark, scala, kafka
 brew cask install java
-brew install scala
-brew install sbt
-brew install apache-spark
-brew install kafka
+brew install go scala sbt apache-spark kafka
+
+# Install image and audio software
+brew install gwyddion epstool imagemagick graphviz ffmpeg libsndfile poppler
 
 # Node/npm
 brew install node
-npm install npm@latest -g
+npm install -g npm@latest
 npm install -g n
 npm install -g typescript
 
-# OpenSSL
-brew install openssl
-
 # Protobuf
-brew install protobuf
-brew install grpcurl
-brew install bufbuild/buf/buf
+brew install protobuf grpcurl bufbuild/buf/buf
 
 # Terraform
 brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
 
 # Other useful tools
-brew install p7zip
-brew install xz
-brew install speedtest-cli
-brew install htop
-brew install btop
-brew install tree
-brew install nmap
-brew install hadolint
-brew install hdf5
-brew install poppler
-brew install rename
+brew install p7zip xz speedtest-cli bat htop btop tree nmap hadolint hdf5 rename fzf yq
 
 # Cleanup
 brew cleanup -s
